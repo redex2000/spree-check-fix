56fe6f7fa8e5d8153e4137eb3511b4f181f0d2f8
Torsten Rüger <torsten@lightning.nu>
2011-01-15 18:39:04 +0200

    fixes stack problem + typo

--- app/models/payment_decorator.rb
+++ app/models/payment_decorator.rb
@@ -1,10 +1,16 @@
 Payment.class_eval do
 
-  alias old_actions actions
+  #used to alias, but that extra call pushes my stack over the limit   
+  #alias old_actions actions
   
   def actions
-    return old_actions if payment_source
-    ["pay"]
+    ret = [] 
+    if payment_source and payment_source.respond_to? :actions
+      ret = payment_source.actions.select { |action| !payment_source.respond_to?("can_#{action}?") or payment_source.send("can_#{action}?", self) }
+    else
+      ret << "pay"  if payment_source == nil and state != "completed" 
+    end
+    ret
   end
   
   def pay
